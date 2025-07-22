0b48e226323e7a0584d7a5042f66ea4d2fb26408
cathy-coder <osorecathy@gmail.com>
2025-07-22 05:23:09 +0300

    added API functionality

--- scr/index.js
+++ scr/index.js
@@ -1,15 +1,33 @@
+function refreshWeather(response){
+    let temperatureElement =document.querySelector("temperature");
+    let temperature =response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML =Response.data.city;
+    temperatureElement.innerHTML = Math.round (temperature);
+}
+
+
+function searchCity(city) {
+    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(refreshWeather);
+    
+  }
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector(".search-form-input");
     let cityElement = document.querySelector("#city");
     
     cityElement.innerHTML = searchInput.value;
-    console.log()
+    let apiUrl = searchCity(searchInput.value);
+    
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-function searchCity(kenya){
+function searchCity(paris){
 
 }
